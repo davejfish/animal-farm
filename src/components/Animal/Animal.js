33b2d81bdbc2d2d9241e2129e3a63f08f1de03b3
Dave Fisher <dave.j.fish@gmail.com>
2022-09-12 15:22:07 -0700

    working half baked

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,11 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+export default function Animal(props) {
+  return (
+    <div className='animal' style={{ top: props.top, left: props.left }} >
+      <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+      <span>{props.name}</span>
+      <span>{props.says}</span>
+    </div>
+  );
 }
